77df9289797dbdbbbc34a7f97aabea239acc0fec
Samppa-code <60503282+Samppa-code@users.noreply.github.com>
2023-02-04 23:26:04 +0200

    Made come similar and added one more line in deve

--- git_test.py
+++ git_test.py
@@ -2,5 +2,6 @@
 # Author:  Samu Nordman
 #Description: Code for git testing purposes
 
-print("I am somewhat familiar with Git. Added some more text to print here")
-print("Addes some code to development")
+print("I am somewhat familiar with Git. Added some more text to print here.")
+print("Added a new print") 
+print("Development line")
