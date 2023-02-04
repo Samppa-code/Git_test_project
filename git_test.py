c5e39d36dda7c2e713b473acc771f8420d909408
Samppa-code <60503282+Samppa-code@users.noreply.github.com>
2023-02-04 16:23:09 +0200

    Added new code to print

--- git_test.py
+++ git_test.py
@@ -2,4 +2,5 @@
 # Author:  Samu Nordman
 #Description: Code for git testing purposes
 
-print("I am somewhat familiar with Git. Added some more text to print here") 
+print("I am somewhat familiar with Git. Added some more text to print here")
+print("Addes some code to development")
